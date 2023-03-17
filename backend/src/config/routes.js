885fc188cbdf351a55a041ce937a136ce22a7d72
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-17 09:12:48 -0300

    enabled cors.

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 module.exports = function(server) {
     // API Routes
     const router = express.Router();
-    server.use('/api', router);
+    server.user('/api', router);
 
     // TODO Routes
     const todoService = require('../api/todo/todoService');
